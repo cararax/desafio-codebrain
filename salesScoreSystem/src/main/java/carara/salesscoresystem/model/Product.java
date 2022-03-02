f2067b3894ea601d9223ec62c7e0d879ed15a967
Pedro Carara <cararax@gmail.com>
2022-03-02 19:15:48 -0300

    feat: product model and repository added

---
+++ salesScoreSystem/src/main/java/carara/salesscoresystem/model/Product.java
@@ -0,0 +1,24 @@
+package carara.salesscoresystem.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private Double price;
+}
