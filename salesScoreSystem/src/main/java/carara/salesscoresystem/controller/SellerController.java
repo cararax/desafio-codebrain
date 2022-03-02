c153a3d8608a7c35d60c2ea83c18c4f62b941457
Pedro Carara <cararax@gmail.com>
2022-03-02 20:39:35 -0300

    feat: endpoint to update seller added

--- salesScoreSystem/src/main/java/carara/salesscoresystem/controller/SellerController.java
+++ salesScoreSystem/src/main/java/carara/salesscoresystem/controller/SellerController.java
@@ -1,7 +1,9 @@
 package carara.salesscoresystem.controller;
 
+import carara.salesscoresystem.dto.ProductDto;
 import carara.salesscoresystem.model.Seller;
 import carara.salesscoresystem.service.SellerService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +23,18 @@ public class SellerController {
     @PostMapping
     public ResponseEntity<Seller> insertSeller(@RequestBody Seller seller) {
         return ResponseEntity.status(HttpStatus.CREATED).body(sellerService.insertSeller(seller));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateSeller(@PathVariable Long id, @RequestBody ProductDto productDto) {
+        Optional<Seller> productToUpdate = sellerService.findById(id);
+        if (productToUpdate.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Seller not found.");
+        }
+        Seller seller = productToUpdate.get();
+        BeanUtils.copyProperties(productDto, seller);
+        seller.setId(id);
+        return ResponseEntity.status(HttpStatus.OK).body(sellerService.insertSeller(seller));
     }
 
     @DeleteMapping("/{id}")
