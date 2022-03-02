8f14177c97bdd157518d591b4d4b7987482face8
Pedro Carara <cararax@gmail.com>
2022-03-02 20:24:10 -0300

    feat: seller model and repository added

---
+++ salesScoreSystem/src/main/java/carara/salesscoresystem/repository/SellerRepository.java
@@ -0,0 +1,7 @@
+package carara.salesscoresystem.repository;
+
+import carara.salesscoresystem.model.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+}
