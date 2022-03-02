9215cf5ca8cf4b9f7619cce3b06d41c16c03ba75
Pedro Carara <cararax@gmail.com>
2022-03-02 18:44:21 -0300

    Project started 🥳

---
+++ salesScoreSystem/src/main/java/carara/salesscoresystem/SalesScoreSystemApplication.java
@@ -0,0 +1,13 @@
+package carara.salesscoresystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesScoreSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesScoreSystemApplication.class, args);
+    }
+
+}
---
+++ salesScoreSystem/src/test/java/carara/salesscoresystem/SalesScoreSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package carara.salesscoresystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesScoreSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
