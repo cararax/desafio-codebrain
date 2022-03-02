05773b893f2d5a41e9a67db49eeee91d87b0b857
Pedro Carara <cararax@gmail.com>
2022-03-02 19:29:45 -0300

    feat: endpoint to delete product added

--- salesScoreSystem/src/main/java/carara/salesscoresystem/controller/ProductController.java
+++ salesScoreSystem/src/main/java/carara/salesscoresystem/controller/ProductController.java
@@ -5,10 +5,9 @@ import carara.salesscoresystem.model.Product;
 import carara.salesscoresystem.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
@@ -25,4 +24,13 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.insertProduct(productDto));
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
+        Optional<Product> productToDelete = productService.findById(id);
+        if (productToDelete.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        productService.deleteProduct(productToDelete.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully.");
+    }
 }
--- salesScoreSystem/src/main/java/carara/salesscoresystem/service/ProductService.java
+++ salesScoreSystem/src/main/java/carara/salesscoresystem/service/ProductService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -18,9 +19,19 @@ public class ProductService {
     }
 
     @Transactional
-    public Object insertProduct(ProductDto productDto) {
+    public Product insertProduct(ProductDto productDto) {
         Product newProduct = new Product();
         BeanUtils.copyProperties(productDto, newProduct);
         return productRepository.save(newProduct);
+    }
+
+    @Transactional
+    public void deleteProduct(Product product) {
+        productRepository.delete(product);
+
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
     }
 }
